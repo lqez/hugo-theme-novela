e2318a8a0dd6239a124d075c8e4a13c3d38e1159
Park Hyunwoo <ez.amiryo@gmail.com>
2020-02-07 12:51:28 +0900

    Simplifying dark mode

--- assets/js/toggleColors.js
+++ assets/js/toggleColors.js
@@ -4,6 +4,25 @@ let bodyElement = document.getElementsByTagName("BODY")[0];
 let moonOrSun = document.getElementById("moonOrSun");
 let sunRays = document.getElementById("sunRays");
 let moonMask = document.getElementById("moonMask");
+let variables = [
+    'primary',
+    'secondary',
+    'grey',
+    'background-color',
+    'accent',
+    'hover',
+    'gradient',
+    'articleText',
+    'track',
+    'progress',
+    'card',
+    'error',
+    'success',
+    'errorBackground',
+    'horizontalRule',
+    'inputBackground',
+    'tooltip',
+];
 
 initAnimation();
 
@@ -64,41 +83,13 @@ function toggleColors(){
 }
 
 function makeDark(){
-    bodyElement.style.setProperty('--primary', "#fff");
-    bodyElement.style.setProperty('--secondary', "#fff");
-    bodyElement.style.setProperty('--grey', "#73737D");
-    bodyElement.style.setProperty('--background-color', "#111216");
-    bodyElement.style.setProperty('--accent', "#E9DAAC");
-    bodyElement.style.setProperty('--hover', "rgba(255, 255, 255, 0.07)");
-    bodyElement.style.setProperty('--gradient', "linear-gradient(180deg, #111216 0%, rgba(66, 81, 98, 0.36) 100%)");
-    bodyElement.style.setProperty('--articleText', "#fff");
-    bodyElement.style.setProperty('--track', "rgba(255, 255, 255, 0.3)");
-    bodyElement.style.setProperty('--progress', "#fff");
-    bodyElement.style.setProperty('--card', "#1D2128");
-    bodyElement.style.setProperty('--error', "#EE565B");
-    bodyElement.style.setProperty('--success', "#46B17B");
-    bodyElement.style.setProperty('--errorBackground', "rgba(238, 86, 91, 0.1)");
-    bodyElement.style.setProperty('--horizontalRule', "rgba(255, 255, 255, 0.15)");
-    bodyElement.style.setProperty('--inputBackground', "rgba(255, 255, 255, 0.07)");
-    bodyElement.style.setProperty('--tooltip', "#000");
+    variables.forEach(key => {
+      bodyElement.style.setProperty('--' + key, 'var(--dark-' + key + ')');
+    });
 }
 
 function makeLight(){
-    bodyElement.style.setProperty('--primary', "");
-    bodyElement.style.setProperty('--secondary', "");
-    bodyElement.style.setProperty('--grey', "");
-    bodyElement.style.setProperty('--background-color', "");
-    bodyElement.style.setProperty('--accent', "");
-    bodyElement.style.setProperty('--hover', "");
-    bodyElement.style.setProperty('--gradient', "");
-    bodyElement.style.setProperty('--articleText', "");
-    bodyElement.style.setProperty('--track', "");
-    bodyElement.style.setProperty('--progress', "");
-    bodyElement.style.setProperty('--card', "");
-    bodyElement.style.setProperty('--error', "");
-    bodyElement.style.setProperty('--success', "");
-    bodyElement.style.setProperty('--errorBackground', "");
-    bodyElement.style.setProperty('--horizontalRule', "");
-    bodyElement.style.setProperty('--inputBackground', "");
-    bodyElement.style.setProperty('--tooltip', "lightgrey");
+    variables.forEach(key => {
+      bodyElement.style.setProperty('--' + key, '');
+    });
 }
